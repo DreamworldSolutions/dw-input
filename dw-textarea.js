aa50e2d429c8c67730952ee686e229da7b5c2f4c
Nirmal Baldaniya <nirmalb@codetailor.in>
2020-11-04 14:37:46 +0530

    fix(dw-textarea): fix performance issue while autogrow.

--- dw-textarea.js
+++ dw-textarea.js
@@ -10,6 +10,25 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 import { LitElement, html, css } from 'lit-element';
 import { styleMap } from 'lit-html/directives/style-map.js';
+
+/**
+ * Behaviors:
+ *  - Provides way to undecorate textarea. 
+ *  - When minimum height is provided, it ocuupies that provided mminum height.
+ *  - When maximum height is provided & content is larger than it, it produces scroll of itself.
+ *  - When nothing provided by integrator (minimum / maximum height), It autogrows based on content.
+ *    - Note: To apply autogrow feature, hack is applied here. 
+ *      - It contains one dummy text field to calculate height of content.
+ *      - Why this hack needed?
+ *        - To auto grow & auto shrink height of textarea based on it's content, there is no way to calculate content's height into textarea.
+ *          So to achieve this, We have attached 1 dummy textarea with single line & set it's value. From that dummy textarea, 
+ *          We can compute it's scrollHeight & set height of actaul visible textarea to that scrollHeight.
+ * 
+ * 
+ * Usage pattern:
+ *  - `<dw-textarea undecorated .minHeight=${56} .maxHeight=${128} ></dw-textarea>`
+ */
+
 export class DwTextarea extends LitElement {
   static get styles() {
     return [
@@ -20,6 +39,7 @@ export class DwTextarea extends LitElement {
           -moz-box-sizing: border-box;
           box-sizing: border-box;
           color: var(--mdc-theme-text-primary);
+          position: relative;
         }
 
         :host[hidden] {
@@ -80,6 +100,13 @@ export class DwTextarea extends LitElement {
         :host([undecorated]), 
         :host([undecorated]) textarea {
           border: none;
+        }
+
+        #dummy-textarea {
+          position: absolute;
+          top: -99999px;
+          height: auto;
+          visibility: hidden;
         }
       `
     ];
@@ -167,6 +194,10 @@ export class DwTextarea extends LitElement {
     });
   }
 
+  get _dummyTextarea() {
+    return this.renderRoot.querySelector('#dummy-textarea');
+  }
+
   render() {
     return html`<textarea id="textarea" rows="1"
         style=${styleMap(this._textareaStyle())}
@@ -182,7 +213,10 @@ export class DwTextarea extends LitElement {
         @cut="${this._resize}"
         @paste="${this._resize}"
         @keypress="${this._onKeyPress}"
-        @keydown="${this._onKeyDown}"></textarea>`;
+        @keydown="${this._onKeyDown}"></textarea>
+        
+        <textarea .value="${this.value}" rows="1" disabled id="dummy-textarea"></textarea>
+        `;
   }
 
   constructor() {
@@ -305,12 +339,11 @@ export class DwTextarea extends LitElement {
    * @protected
    */
   _resize() {
-    if(!this._textarea || this.minHeight === this.maxHeight) {
+    if(!this._textarea || !this._dummyTextarea || this.minHeight === this.maxHeight) {
       return;
     }
 
-    this.style.height = this._textarea.style.height = 'auto';
-    let scrollHeight = this._textarea.scrollHeight;
+    const scrollHeight = this._dummyTextarea.scrollHeight;
     if(scrollHeight < this.minHeight) {
       this.style.height = this._textarea.style.height = this.minHeight + 'px';
       return;
