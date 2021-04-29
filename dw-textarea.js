e5ffa8957fd80a008886ef3f347478510191b50f
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-04-29 09:58:33 +0530

    fix(dw-textarea): fix textarea undefined error.

--- dw-textarea.js
+++ dw-textarea.js
@@ -203,6 +203,10 @@ export class DwTextarea extends LitElement {
     return this.renderRoot.querySelector('#dummy-textarea');
   }
 
+  get _textarea() {
+    return this.renderRoot.querySelector('#textarea');
+  }
+
   render() {
     return html`<textarea id="textarea" rows="1"
         style=${styleMap(this._textareaStyle())}
@@ -236,35 +240,11 @@ export class DwTextarea extends LitElement {
     this.minHeight = 42;
     this.maxLength = 524288;
     this.minLength = 0;
-
-    /**
-     * A reference to the textarea element.
-     */
-    this._textarea = null;
-  }
-
-  connectedCallback() {
-    super.connectedCallback && super.connectedCallback();
-
-    //Set textarea instance.
-    this.updateComplete.then(() => {
-      this._textarea = this.shadowRoot.querySelector('#textarea');
-    });
   }
 
   firstUpdated(changeProps) {
     super.firstUpdated && super.firstUpdated(changeProps);
-
-    //If textarea instance is not set.
-    if(!this._textarea) {
-      this._textarea = this.shadowRoot.querySelector('#textarea');
-    }
     this._resize();
-  }
-
-  disconnectedCallback() {
-    this._textarea = undefined;
-    super.disconnectedCallback && super.disconnectedCallback();
   }
 
   /**
@@ -459,10 +439,6 @@ export class DwTextarea extends LitElement {
    * @protected
    */
   _onInput() {
-    if (this._textarea === undefined) {
-      console.warn('dw-textarea: Element is disconnected before `input` event handler call.');
-      return;
-    }
     let value = this._textarea.value;
     if(value && this.disabledEnter) {
       value = value.replace(/(\r\n|\n|\r)/gm, " ");
@@ -481,10 +457,6 @@ export class DwTextarea extends LitElement {
    * @protected
    */
   _onInputBlur(e) {
-    if (this._textarea === undefined) {
-      console.warn('dw-textarea: Element is disconnected before `blur` event handler call.');
-      return;
-    }
     this.dispatchEvent(new CustomEvent('blur', {
       detail: { value: this._textarea.value, event: e }
     }));
